9396969b7b8ac92c9e0a9f3e8437af8c1356afc5
PHProger <bogdan.raguzin@gmail.com>
2021-07-12 19:35:09 +0300

    Users and Messages tables migrations added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,18 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('nickname');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('color');
+            $table->string('status');
+            $table->tinyInteger('is_editor');
+            $table->string('last_online');
+            $table->string('background');
+            $table->tinyInteger('implicit');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2021_07_12_160422_create_chat_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chat', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->unsigned();
+            $table->text('message');
+            $table->tinyInteger('is_pinned')->default(0);
+            $table->tinyInteger('to_bot')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chat');
+    }
+}
